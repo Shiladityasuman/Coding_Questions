6ad8e05812f1adb3d3ff1737081e69269d13b281
Shiladitya Suman <134432207+Shiladityasuman@users.noreply.github.com>
2024-11-08 00:44:59 +0530

    Inverted pattern

---
+++ inverted_star.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class inverted_star {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        System.out.println("Enter the number of lines u want:");
+        int n = sc.nextInt();
+        for(int line=1; line<=n; line++){
+            for(int star=1; star<=n-line+1; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
